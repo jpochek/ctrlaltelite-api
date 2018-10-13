61ea4632a30415a85fa445b4772d3396a1691dd7
John R. Hicks <john@determinato.com>
2018-10-13 17:52:52 -0400

    Add method to VolunteerProfileRepository to support service requests

--- src/main/java/com/charter/interpretme/repository/VolunteerProfileRepository.java
+++ src/main/java/com/charter/interpretme/repository/VolunteerProfileRepository.java
@@ -2,10 +2,16 @@ package com.charter.interpretme.repository;
 
 import com.charter.interpretme.rest.entity.VolunteerProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface VolunteerProfileRepository extends JpaRepository<VolunteerProfile, String> {
+public interface VolunteerProfileRepository extends JpaRepository<VolunteerProfile, String>, JpaSpecificationExecutor {
     VolunteerProfile findByUsername(String username);
+
     VolunteerProfile findByEmailAddress(String emailAddress);
+
+    List<VolunteerProfile> findByStreetAddress1AndStateAndPostalCode(String streetAddress, String state, String postalCode);
 }
